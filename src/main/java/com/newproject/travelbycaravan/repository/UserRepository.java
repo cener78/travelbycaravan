09195a5ab689ecd1362e33ae02c718bc253ccff9
cener78 <94464442+cener78@users.noreply.github.com>
2022-08-10 00:35:07 +0200

    new commit

--- src/main/java/com/newproject/travelbycaravan/repository/UserRepository.java
+++ src/main/java/com/newproject/travelbycaravan/repository/UserRepository.java
@@ -3,10 +3,18 @@ package com.newproject.travelbycaravan.repository;
 
 import com.newproject.travelbycaravan.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Optional;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    //@Query("Select u From User Where u.email=?1")
+    Optional<User> findByEmail(String email);
 
 }
---
+++ src/main/java/com/newproject/travelbycaravan/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.newproject.travelbycaravan.security.service;
+
+import com.newproject.travelbycaravan.domain.User;
+import com.newproject.travelbycaravan.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+   private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user=userRepository.findByEmail(email)
+                .orElseThrow(()->new UsernameNotFoundException("User not found with email: "+email));
+        return UserDetailsImpl.build(user);
+    }
+}
