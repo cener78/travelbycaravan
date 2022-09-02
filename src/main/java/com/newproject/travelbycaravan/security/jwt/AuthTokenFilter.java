ccc0be216668eec6e5a72789fb9cc051025a1afe
cener78 <94464442+cener78@users.noreply.github.com>
2022-09-02 16:24:10 +0200

    Authentication problems are handled

--- src/main/java/com/newproject/travelbycaravan/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/newproject/travelbycaravan/security/jwt/AuthTokenFilter.java
@@ -3,11 +3,13 @@ package com.newproject.travelbycaravan.security.jwt;
 import com.newproject.travelbycaravan.domain.User;
 import com.newproject.travelbycaravan.repository.UserRepository;
 import com.newproject.travelbycaravan.security.service.UserDetailsServiceImpl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -18,6 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
+
 
 public class AuthTokenFilter extends OncePerRequestFilter {
 
@@ -30,41 +33,40 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-
         try {
-            String jwt=parseJwt(request);
-            if (jwt!=null&&jwtUtils.validateJwtToken(jwt)){
-                Long id=jwtUtils.getIdFromJwtToken(jwt);
-                request.setAttribute("id",id);
-                Optional<User>user =userRepository.findById(id);
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                Long id = jwtUtils.getIdFromJwtToken(jwt);
+                request.setAttribute("id", id);
+                Optional<User> user = userRepository.findById(id);
 
-                UserDetails userDetails= userDetailsService.loadUserByUsername(user.get().getEmail());
-
-                UsernamePasswordAuthenticationToken authentication =new
-                        UsernamePasswordAuthenticationToken(userDetails, null,userDetails.getAuthorities());
+                UserDetails userDetails = userDetailsService.loadUserByUsername(user.get().getEmail());
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                SecurityContextHolder.createEmptyContext().setAuthentication(authentication);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
-        }catch(Exception e){
-            logger.error("Cannot set user authentication: {}",e);
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
         }
 
-        filterChain.doFilter(request,response);
-
+        filterChain.doFilter(request, response);
     }
-    private String parseJwt(HttpServletRequest request){
-        String headerAuth=request.getHeader("Authorization");
 
-        if(StringUtils.hasText(headerAuth)&& headerAuth.startsWith("Bearer ")){
-            return  headerAuth.substring(7);
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
         }
 
-        return  null;
-
+        return null;
     }
 
 }
