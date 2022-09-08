bcfa527343ead412981f19c5ed291ad6e75842c4
cener78 <94464442+cener78@users.noreply.github.com>
2022-09-08 23:47:05 +0200

    caravans table issue handled

--- src/main/java/com/newproject/travelbycaravan/domain/Caravan.java
+++ src/main/java/com/newproject/travelbycaravan/domain/Caravan.java
@@ -4,73 +4,82 @@ package com.newproject.travelbycaravan.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.validator.constraints.Length;
-
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.util.Set;
 
-@NoArgsConstructor
-@Getter
 @Setter
+@Getter
+@NoArgsConstructor
 @Entity
-@Table(name="caravans")
-public class Caravan   {
+@Table(name = "caravans")
+public class Caravan implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
-    @Size(max = 30,message = "Size is exceeded")
-    @NotNull(message = "please enter the caravan model")
-    @Column(length = 30,nullable = false)
+    @Size(max = 30, message = "Size is exceeded")
+    @NotNull(message = "Please enter the car model")
+    @Column(length = 30, nullable = false)
     private String model;
 
-    @NotNull(message = "Please enter the caravan beds")
+    @NotNull(message = "Please enter the car doors")
     @Column(nullable = false)
     private Integer beds;
 
-    @NotNull(message = "Please enter the caravan seats")
+    @NotNull(message = "Please enter the car seats")
     @Column(nullable = false)
     private Integer seats;
 
-    @NotNull(message = "Please enter the caravan luggage")
+    @NotNull(message = "Please enter the car luggage")
     @Column(nullable = false)
-    private String luggage;
+    private Integer luggage;
 
-    @Size(max=30)
-    @NotNull(message = "Please enter the caravan transmission")
+    @Size(max = 30, message = "Size is exceeded")
+    @NotNull(message = "Please enter the car transmission")
     @Column(length = 30, nullable = false)
     private String transmission;
 
-    @NotNull(message = "Please enter the caravan air conditioning")
+    @NotNull(message = "Please enter the car air conditioning")
     @Column(nullable = false)
     private Boolean airConditioning;
 
-    @NotNull(message = "Please enter the caravan age")
+    @NotNull(message = "Please enter the car age")
     @Column(nullable = false)
     private Integer age;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "caravan_image",
+            joinColumns = @JoinColumn(name = "caravan_id"),
+            inverseJoinColumns = @JoinColumn(name = "file_id"))
+    private Set<FileDB> image;
 
-    @NotNull(message = "Please enter the caravan price")
+    @NotNull(message = "Please enter price per hour of the car")
     @Column(nullable = false)
     private Double pricePerDay;
 
-    @Size(max=30)
-    @NotNull(message = "Please enter the caravan fuel type")
-    @Column(length = 30, nullable = false)
+    @Size(max = 30, message = "Size is exceeded")
+    @NotNull(message = "Please enter the car fuel type")
+    @Column(nullable = false, length = 30)
     private String fuelType;
 
-    private Boolean builtIn=false;
+    private Boolean builtIn;
 
-    @ManyToMany(fetch=FetchType.LAZY)
-    @JoinTable(name="caravan_image",
-            joinColumns = @JoinColumn(name="car_id")
-            ,inverseJoinColumns = @JoinColumn(name="file_id"))
-
-    private Set<FileDB> image;
-
-
+    public Caravan(String model, Integer doors, Integer seats, Integer luggage, String transmission,
+               Boolean airConditioning, Integer age, Set<FileDB> image, Double pricePerDay, String fuelType) {
+        this.model = model;
+        this.beds = doors;
+        this.seats = seats;
+        this.luggage = luggage;
+        this.transmission = transmission;
+        this.airConditioning = airConditioning;
+        this.age = age;
+        this.image = image;
+        this.pricePerDay = pricePerDay;
+        this.fuelType = fuelType;
+    }
 }
